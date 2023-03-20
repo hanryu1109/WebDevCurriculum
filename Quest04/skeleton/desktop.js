10735ad1bf87fbb97b2e285e3ce36e1c118516c5
Areum Han <hanryu1109@gmail.com>
2023-03-20 19:22:24 +0900

    feature: Quest04. OOP의 기본 퀘스트 진행 중

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -1,12 +1,14 @@
 class Desktop {
   /* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
   // init: 처음에 생겨날 아이콘과 폴더의 개수를 받을 수 있습니다.
-  // 여러 개의 바탕화면을 각각 다른 DOM 엘리먼트에서 동시에 운영할 수 있다고???
+  // 여러 개의 바탕화면을 각각 다른 DOM 엘리먼트에서 동시에 운영할 수 있기 때문에 어떤 dom element인지 알아야 합니다.
   // 변수로는: 폴더 갯수, 아이콘 갯수, makeFolder, makeIcon
+  #numOfFolder;
+  #numOfIcon;
 
   constructor(numOfFolder, numOfIcon) {
-    this.numOfFolder = numOfFolder;
-    this.numOfIcon = numOfIcon;
+    this.#numOfFolder = numOfFolder;
+    this.#numOfIcon = numOfIcon;
   }
 }
 
