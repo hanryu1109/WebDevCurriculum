886b9d8ec00e242e5ec963962e20d3a8d6d17c98
Areum Han <hanryu1109@gmail.com>
2023-03-23 16:46:31 +0900

    init: Quest08. 웹 API의 기초 Quest 초기 세팅

--- Quest08/skeleton/server.mjs
+++ Quest08/skeleton/server.mjs
@@ -1,8 +1,13 @@
-import http from 'http';
+import http from "http";
+
+const port = 8080;
 
 const server = http.createServer((req, res) => {
-    /* TODO: 각각의 URL들을 어떻게 처리하면 좋을까요? */
-    res.end();
+  /* TODO: 각각의 URL들을 어떻게 처리하면 좋을까요? */
+  /* createServer의 콜백함수에서 req.url 을 통해 URL을 확인하여 각각의 URL에 대응하는 응답을 처리하면 좋을 것 같습니다.  */
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!!!\n");
 });
 
-server.listen(8000);
+server.listen(port);
