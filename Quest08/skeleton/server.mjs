028dd59f6160ac0c76a930eb36681557c08c5b9e
Areum Han <hanryu1109@gmail.com>
2023-03-23 17:10:08 +0900

    feature: Quest08. /foo URL의 GET 요청에 대한 응답

--- Quest08/skeleton/server.mjs
+++ Quest08/skeleton/server.mjs
@@ -1,13 +1,25 @@
 import http from "http";
+import url from "url";
 
 const port = 8080;
 
 const server = http.createServer((req, res) => {
   /* TODO: 각각의 URL들을 어떻게 처리하면 좋을까요? */
   /* createServer의 콜백함수에서 req.url 을 통해 URL을 확인하여 각각의 URL에 대응하는 응답을 처리하면 좋을 것 같습니다.  */
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!!!\n");
+
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-Type", "text/plain");
+  //   res.end("Hello, World!!!\n");
+  const parsedUrl = url.parse(request.url);
+  const pathName = parsedUrl.pathname;
+
+  if (req.method === "GET" && pathName === "/foo") {
+    const queryString = url.parse(req.url, true).query;
+    const string = queryString.bar ? queryString.bar : "";
+
+    res.setHeader("Content-Type", "text/html");
+    res.end(`GET method!!! ${string}\n`);
+  }
 });
 
 server.listen(port);
